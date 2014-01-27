38e15a5b8a3e495fa72feaac3cf5479c8bdecbf5
Liz Abinante <eabinante@gmail.com>
2014-01-27 13:08:14 -0600

    split stuff up so i can jsut edit the load file for now - probably need to make this into a rake task

---
+++ _plugins/_load.rb
@@ -0,0 +1,13 @@
+require_relative 'api.rb'
+require_relative 'collection.rb'
+require_relative 'pattern_ids.rb'
+
+# Super hacky, but uncomment these lines to seed.
+
+# ALL PATTERNS
+# pattern_ids.each do |id|
+#   RavelryPattern.new(id)
+# end
+
+# ALL COLLECTION PAGES
+# collections.each { |collection| collection.create_file }
