cd91688157842d8bb2d32f8b697054996d1d7f7a
Liz Abinante <eabinante@gmail.com>
2016-02-09 18:16:06 -0800

    look a controller

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+require('../../node_modules/angular/angular')
+require('../../node_modules/angular-ui-router/build/angular-ui-router')
+
+var _ = require('lodash')
+
+var feministy = angular.module('feministy', [
+  // angular
+  'ui.router',
+])
+
+// main controller
+feministy.controller('feministyController', function($scope) {
+  // stuff
+})
