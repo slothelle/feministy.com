74bde7906036c9cb1c964c48621fa306abd74123
Liz Abinante <eabinante@gmail.com>
2016-03-06 12:20:41 -0800

    nested route view magic, simple styles for pattern page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,54 @@
 require('../../node_modules/angular/angular')
-require('../../node_modules/angular-ui-router/build/angular-ui-router')
+require('../../node_modules/angular-ui-router/release/angular-ui-router')
 
 var _ = require('lodash')
 
-var feministy = angular.module('feministy', [
+var feministyDotCom = angular.module('feministyDotCom', [
   // angular
   'ui.router',
 ])
 
+feministyDotCom.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    views: {
+      "landing-page": {
+        templateUrl: 'templates/landing-page.html'
+      }
+    },
+    url: '/'
+  }).state('patterns', {
+    views: {
+      "nav": {
+        templateUrl: 'templates/menu.html'
+      },
+      "header": {
+        templateUrl: 'templates/header_submenu.html'
+      },
+      "content": {
+        templateUrl: 'templates/patterns.html'
+      }
+    },
+    url: '/patterns'
+  }).state('category', {
+    views: {
+      "nav": {
+        templateUrl: 'templates/menu.html'
+      },
+      "header": {
+        templateUrl: 'templates/header_submenu.html'
+      },
+      "content@": {
+        templateUrl: function ($stateParams) {
+          return 'templates/category_' + $stateParams.id + '.html'
+        }
+      }
+    },
+    url: '/patterns/category/:id'
+  })
+  $urlRouterProvider.otherwise('/');
+})
+
 // main controller
-feministy.controller('feministyController', function($scope) {
-  // stuff
+feministyDotCom.controller('mainController', function($scope) {
+  // probably stuff
 })
