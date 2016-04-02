a00874f4f19b130594840afd306782436511506c
Liz Abinante <eabinante@gmail.com>
2016-04-02 08:03:35 -0700

    modify script to generate category pages, clean up markup, start working on single pattern pages

--- scripts/app.rb
+++ scripts/app.rb
@@ -36,17 +36,42 @@ end
 
 puts "Done fetching!"
 
-template = "<div class='columns four'>NEWLINE<a href='URL'><img src='IMAGE_URL'></a>NEWLINE</div>NEWLINE"
-template_blob = []
+template = "<div class='row'><div class='columns four'><a href='URL'><img src='IMAGE'></a></div><div class='columns eight'><h2><a href='URL'>PATTERN_NAME</a></h2><p><strong>projects: </strong>PROJECT_COUNT</p><p><strong>favorites: </strong>FAVORITE_COUNT</p><p><strong>queues: </strong>QUEUED_COUNT</p></div></div>NEWLINE"
 
-patterns.each_with_index do |pattern, index|
-  index += 1
+pattern_template = '<!DOCTYPE html><!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]--><!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]--><!--[if IE 8]><html class="no-js lt-ie9"> <![endif]--><!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]--><head><base href="/"><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>feministy.com - uniquely simple knitwear design by liz abinante</title><meta name="description" content=""><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="css/main.css"><!-- Place favicon.ico and apple-touch-icon.png in the root directory --></head><body ng-app="feministyDotCom"><div id="landing-page" ui-view="landing-page"><!-- pretty landing page stuff! --></div><div id="nav" class="row" ui-view="nav"></div><div class="container">    <div id="header" class="row" ui-view="header"></div>    <div id="content" class="row" ui-view="content"><div id="pattern"><h1 class="centered">PATTERN_NAME</h1>   <div class="row"><div class="columns six centered"><img src="IMAGEFIRST"></div><div class="columns six centered"><img src="IMAGESECOND"></div></div>       ***STUFF*****     </div></div></div><!--[if lt IE 7]>    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]--><script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script><script>window.jQuery || document.write(\'<script src="js/jquery.min.js"><\/script>\')</script><script src="js/index.js"></script></body></html>'
+
+template_blob = []
+shawls_blob = []
+cowls_blob = []
+socks_blob = []
+hats_blob = []
+
+patterns.sort_by { |pattern| pattern.projects_count }.reverse.each do |pattern|
   permalink = pattern.permalink.split('/').last
-  temp = template.sub('URL', "/#{permalink}")
-  temp = temp.sub('IMAGE_URL', pattern.photos.first.small_url)
-  temp += 'NEWROW' if index % 3 == 0
+  temp = template.gsub('URL', "/#{permalink}")
+  temp = temp.sub('IMAGE', pattern.photos.first.small_url)
+  temp = temp.sub('PATTERN_NAME', pattern.name.downcase)
+  temp = temp.sub('FAVORITE_COUNT', pattern.favorites_count.to_s.reverse.scan(/\d{1,3}/).join(",").reverse)
+  temp = temp.sub('PROJECT_COUNT', pattern.projects_count.to_s.reverse.scan(/\d{1,3}/).join(",").reverse)
+  temp = temp.sub('QUEUED_COUNT', pattern.queued_projects_count.to_s.reverse.scan(/\d{1,3}/).join(",").reverse)
   template_blob << temp
+
+  if pattern.categories.map(&:name).include?("Shawl / Wrap") || pattern.categories.map(&:name).include?("Scarf")
+    shawls_blob << temp 
+  elsif pattern.categories.map(&:name).include?("Mid-calf")
+    socks_blob << temp 
+  elsif pattern.categories.map(&:name).include?("Cowl")
+    cowls_blob << temp 
+  elsif pattern.categories.map(&:name).include?("Beanie, Toque")
+    hats_blob << temp 
+  else
+    puts pattern.categories.map(&:name)
+  end 
 end
 
-File.open("test.html", "w") { |file| file.write(template_blob.join('')) }
-
+File.open("patterns.html", "w") { |file| file.write(template_blob.join('')) }
+File.open("shawls.html", "w") { |file| file.write(shawls_blob.join('')) }
+File.open("socks.html", "w") { |file| file.write(socks_blob.join('')) }
+File.open("cowls.html", "w") { |file| file.write(cowls_blob.join('')) }
+File.open("hats.html", "w") { |file| file.write(hats_blob.join('')) }
+# File.open("pattern_blob.html", "w") { |file| file.write(pattern_blob) }
